5b8f1da9204e47ed7c5933d1b81031cc10c14da7
Brennon Nuckols <brennuck@gmail.com>
2020-11-17 15:04:18 -0700

    callWeather() working

--- app/weather/weather.component.js
+++ app/weather/weather.component.js
@@ -2,20 +2,26 @@
 
 angular.module("weather").component("weather", {
 	templateUrl: "weather/weather.template.html",
-	controller: ["$http", "$scope", function WeatherController($scope, $http) {
-        $scope.callWeather = function() {
-            $http({
-                method: "GET",
-                url: "https://us-weather-by-zip-code.p.rapidapi.com/getweatherzipcode",
-                params: {zip: '94111'},
-                headers: {
-                    'x-rapidapi-key': '3acd025854mshb88d4616eac0063p19e3a5jsn722c3bcc8795',
-                    'x-rapidapi-host': 'us-weather-by-zip-code.p.rapidapi.com'
-                }
-            }).then((response) => {
-                console.log(response)
-            })
-            console.log("HELLO")
-        }
-    }]
+	controller: [
+		"$http",
+		"$scope",
+		function WeatherController($http, $scope) {
+			$scope.callWeather = function () {
+				$http({
+					method: "GET",
+					url:
+						"https://us-weather-by-zip-code.p.rapidapi.com/getweatherzipcode",
+					params: { zip: "94111" },
+					headers: {
+						"x-rapidapi-key":
+							"3acd025854mshb88d4616eac0063p19e3a5jsn722c3bcc8795",
+						"x-rapidapi-host":
+							"us-weather-by-zip-code.p.rapidapi.com",
+					},
+				}).then((response) => {
+					console.log(response);
+				});
+			};
+		},
+	],
 });
